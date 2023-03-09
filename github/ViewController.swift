b0becfa98b5a562bd8542c74403c59b9943cbd60
Joshua Long <joshlong95@hotmail.co.uk>
2023-03-09 00:47:51 +0000

    view will disappear added

--- github/ViewController.swift
+++ github/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         <#code#>
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        <#code#>
+    }
 
 }
 
